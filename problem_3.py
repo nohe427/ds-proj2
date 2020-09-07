b131be9271e48bebaf50bdfad548b0b14caaa1c1
Alexander Nohe <alex.nohe427@gmail.com>
2020-09-07 17:21:30 +0000

    Pause on Problem 3
    This is how far I have currently gotten with problem three.  Hitting the
    pause button to resume later.

--- problem_3.py
+++ problem_3.py
@@ -1,7 +1,43 @@
 import sys
+## See https://knowledge.udacity.com/questions/227455
+import heapq
 
-def huffman_encoding(data):
-    pass
+class Node(object):
+    def __init__(self, char=None, frequency=None):
+        self.char = char
+        self.frequency = frequency
+        self.left = None
+        self.right = None
+    def __lt__(self, value):
+        return self.frequency < value.frequency
+    def __eq__(self, value):
+        return self.frequency == value.frequency
+    def __str__(self):
+        return f"Character {self.char}, Frequency {self.frequency}"
+    def __repl__(self):
+        return f"Character {self.char}, Frequency {self.frequency}"
+
+def huffman_encoding(data: str):
+    def _enumerate_count_of_characters(data: str) -> dict:
+        chars = {}
+        for char in data:
+            if char in chars.keys():
+                chars[char] += 1
+            else:
+                chars[char] = 1
+        return chars
+    def _sort_chars_to_heap(data: dict) -> list:
+        ## See https://knowledge.udacity.com/questions/227455
+        sorted_list = sorted(data.items(), key = lambda x: x[1])
+        h = []
+        for item in sorted_list:
+            new_node = Node(item[0], item[1])
+            heapq.heappush(h, (item[1], new_node))
+        return h
+    char_count = _enumerate_count_of_characters(data)
+    sorted_chars = _sort_chars_to_heap(char_count)
+    print(sorted_chars)
+    return None, None
 
 def huffman_decoding(data,tree):
     pass
