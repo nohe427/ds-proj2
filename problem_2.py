26d1021053d74c70e8c2bc2c9c967f7238e00358
Alexander Nohe <alex.nohe427@gmail.com>
2020-09-07 15:58:56 +0000

    Problem Two Solved
    This is the solution for problem two. This recursively calls the
    find_files function until all files are found.

--- problem_2.py
+++ problem_2.py
@@ -1,32 +1,32 @@
-## Locally save and call this file ex.py ##
-
-# Code to demonstrate the use of some of the OS modules in python
-
 import os
 
-# Let us print the files in the directory in which you are running this script
-# print (os.listdir("."))
-
-# Let us check if this file is indeed a file!
-# print (os.path.isfile("./ex.py"))
-
-# Does the file end with .py?
-# print ("./ex.py".endswith(".py"))
-
 def find_files(suffix, path):
-    """
-    Find all files beneath path with file name suffix.
+  """
+  Find all files beneath path with file name suffix.
 
-    Note that a path may contain further subdirectories
-    and those subdirectories may also contain further subdirectories.
+  Note that a path may contain further subdirectories
+  and those subdirectories may also contain further subdirectories.
 
-    There are no limit to the depth of the subdirectories can be.
+  There are no limit to the depth of the subdirectories can be.
 
-    Args:
-      suffix(str): suffix if the file name to be found
-      path(str): path of the file system
+  Args:
+    suffix(str): suffix if the file name to be found
+    path(str): path of the file system
 
-    Returns:
-       a list of paths
-    """
-    return None
+  Returns:
+      a list of paths
+  """
+  my_paths = list()
+  for dirpath in os.listdir(path):
+    filepath = os.path.join(path, dirpath)
+    if os.path.isfile(filepath):
+      if filepath.endswith(suffix):
+        my_paths.append(filepath)
+    elif os.path.isdir(filepath):
+      for files in find_files(suffix, filepath):
+        my_paths.append(files)
+  return my_paths
+
+# This is the function invocation.  Comment out if necessary.
+# Problem statement ambigious on if this is requeired in submission.
+print(find_files(".c", "."))
