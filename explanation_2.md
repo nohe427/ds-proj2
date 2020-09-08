3cda6b1c934cf439103c4e48d47d6a6b74a03431
Alexander Nohe <alex.nohe427@gmail.com>
2020-09-08 23:54:01 +0000

    Problem 2 Explanation and Tests

---
+++ explanation_2.md
@@ -0,0 +1,3 @@
+# File Recursion Explanation
+I used a quick check to validate that the path and suffix are not None types so we could work with expected inputs.  I then used a helper function that allowed me to pass a list which I would append my found items to as I went.  This allowed me to make the runtime O(n).
+Runtime O(n)
--- problem_2.py
+++ problem_2.py
@@ -16,17 +16,32 @@ def find_files(suffix, path):
   Returns:
       a list of paths
   """
+  if suffix is None or path is None:
+    return
   my_paths = list()
+  _find_files(suffix, path, my_paths)
+  return my_paths
+
+def _find_files(suffix, path, my_paths):
   for dirpath in os.listdir(path):
     filepath = os.path.join(path, dirpath)
     if os.path.isfile(filepath):
       if filepath.endswith(suffix):
         my_paths.append(filepath)
     elif os.path.isdir(filepath):
-      for files in find_files(suffix, filepath):
-        my_paths.append(files)
-  return my_paths
+      _find_files(suffix, filepath, my_paths)
 
-# This is the function invocation.  Comment out if necessary.
-# Problem statement ambigious on if this is requeired in submission.
-print(find_files(".c", "."))
+# Test Case 1
+print("\n Test 1 - None type file extension")
+print(find_files(None, "."))
+# Retuns nothing
+
+# Test Case 2
+print("\n Test 2 - None type path provided")
+print(find_files(".c", None))
+# Retuns nothing
+
+# Test Case 3
+print("\n Test 3 - Regular input test")
+print(find_files(".py", "."))
+# returns ['./problem_6.py', './problem_3.py', './problem_4.py', './problem_2.py', './problem_1.py', './problem_5.py']
